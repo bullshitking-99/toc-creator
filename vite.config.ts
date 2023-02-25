0fadd9c5582ce17c7fe1f29bc10dbef8dc23da69
李名河 <2219845592@qq.com>
2023-02-25 11:09:06 +0800

    update dist-ghpages ,viteConfig-base

--- vite.config.ts
+++ vite.config.ts
@@ -15,27 +15,29 @@ export default defineConfig({
       mode: [Mode.HTML],
     }),
   ],
+  // 开 gh-pages 需要这玩意儿
+  base: "/toc-creator/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: { open: true },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/export.ts"),
-      name: "TocCreator",
-      fileName: (format) => `TocCreator.${format}.ts`,
-    },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ["vue"],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
-  },
+  // build: {
+  //   lib: {
+  //     entry: path.resolve(__dirname, "src/export.ts"),
+  //     name: "TocCreator",
+  //     fileName: (format) => `TocCreator.${format}.ts`,
+  //   },
+  //   rollupOptions: {
+  //     // 确保外部化处理那些你不想打包进库的依赖
+  //     external: ["vue"],
+  //     output: {
+  //       // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+  //       globals: {
+  //         vue: "Vue",
+  //       },
+  //     },
+  //   },
+  // },
 });
